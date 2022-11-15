c9b669b5adc895368a432a7ab9f9247a2465aafe
samanthacornier <samantha112069@gmail.com>
2022-11-15 15:19:56 -0500

    leaflet paris map

--- public/app.js
+++ public/app.js
@@ -1,19 +1,39 @@
-// create map
+
+                                                    
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
 
-// add openstreetmap tiles
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
-// create and main add geolocation marker
 
 
-// draw the 2nd arrondissement
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
+
+                                    
 
 
-// create red pin marker
+
+const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
-// metro station markers
+
+
 
 
 
